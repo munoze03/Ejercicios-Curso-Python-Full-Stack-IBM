1b0b8dd98944e0e9afc7a84a955bb2726ab47a3b
Enrique <munoze03@gmail.com>
2024-05-13 22:09:24 +0200

    Finalizo la practica

--- Practica Final/Tareas.py
+++ Practica Final/Tareas.py
@@ -1,5 +1,5 @@
 from os import system
-#Creamos la clase tarea con sus Getter y Setter
+#Creamos la clase Tarea con sus Getter y Setter
 class Tarea():
     def __init__(self, descripcion):
         self.descripcion = descripcion
@@ -17,27 +17,12 @@ class Tarea():
     def set_Finalizada(self, finalizada):
         self.finalizada = finalizada
 
-class miErrorEleccion(Exception):
-    def __init__(self, valor):
-        self.valor = valor
-    def __str__(self):
-        return str(self.valor)
+#Declaramos funcion de espera para que el usuario vea la pantalla hasta que pulse intro
+#Para no tener que poner el mismo codigo en varios sitios
+def esperar():
+    input('\n          Pulsa intro para continuar.')
 
-
-#Iniciamos programa
-opcion = 0
-lista = []
-cabecera = '''\n           PROGRAMA DE GESTIÓN DE TAREAS
-             CREADO POR ENRIQUE MUÑOZ
-        ====================================
-        '''
-menu = '''
-          1. Agregar nueva tarea
-          2. Marcar tarea como completada
-          3. Mostrar todas las tareas
-          4. Eliminar una tarea
-          5. Finalizar'''
-
+#Declaramos la funcion de la opcion 1 de introducir una nueva tarea
 def introducirTarea():
     descripcion = input('''
           Introduce la tarea a programar:
@@ -46,31 +31,56 @@ def introducirTarea():
     tarea = Tarea(descripcion)
     lista.append(tarea)
 
+#Declaramos la funcion d la opcion 2 de completar una tarea
+def completarTarea():
+    try:
+        if len(lista) != 0:                             #Controlamos esta opcion por si la lista aun no contiene tareas
+            numTarea = int(input('''
+            Introduce el numero de tarea a completar:
+                
+            '''))
+            if numTarea < 1 or numTarea > len(lista):
+                raise ValueError()                      #Lanzamos una excepcion manualmente si los valores no están entre los indices de la lista
+            else:
+                lista[numTarea-1].set_Finalizada('COMPLETADA')
+                print('\n          Tarea ' + lista[numTarea-1].get_Descripcion() + ' completada')
+                esperar()
+        else:
+            print("          No existen tareas a completar actualmente")
+            esperar()
+    except ValueError:
+        print('\n          Tienes que marcar un numero del 1 al '+str(len(lista)))
+        esperar()
+
+#Declaramos la funcion de la opcion 3 de mostrar todas las tareas
 def mostrasTareas():
     n = 1
     for t in lista:
-        print("        " + str(n) + " " + t.get_Descripcion() , ' - ' , t.get_Finalizada())
+        print("          " + str(n) + " " + t.get_Descripcion() , ' - ' , t.get_Finalizada())
         n += 1
-    # input('\n' + "        Pulsa intro para continuar.")
 
-def completarTarea():
-    numTarea = int(input('''
-        Introduce el numero de tarea a completar:
-        
-        '''))
-    lista[numTarea-1].set_Finalizada('COMPLETADA')
-    print('\n        Tarea ' + lista[numTarea-1].get_Descripcion() + ' completada\n')
-    input('        Pulsa intro para continuar.')
-
+#Declaramos la funcion de la opcion 4 de eliminar una tarea
 def borrarTarea():
-    numTarea = int(input('''
-        Introduce el numero de tarea a eliminar:
-        
-        '''))
-    print('\n        Tarea ' + lista[numTarea-1].get_Descripcion() + ' eliminada\n')
-    lista.pop(numTarea-1)
-    input('        Pulsa intro para continuar.')
-    
+    try:
+        if len(lista) != 0:                             #Controlamos esta opcion por si la lista aun no contiene tareas
+            numTarea = int(input('''
+            Introduce el numero de tarea a eliminar:
+                
+            '''))
+            if numTarea < 1 or numTarea > len(lista):
+                raise ValueError()                      #Lanzamos una excepcion manualmente si los valores no están entre los indices de la lista
+            else:
+                print('\n          Tarea ' + lista[numTarea-1].get_Descripcion() + ' eliminada')
+                lista.pop(numTarea-1)
+                esperar()
+        else:
+            print("          No existen tareas a eliminar actualmente")
+            esperar()
+    except ValueError:
+        print('\n          Tienes que marcar un numero del 1 al '+str(len(lista)))
+        esperar()
+
+#Declaramos la funcion que comprueba la opcion seleccionada
 def comprobarOpcion(opcion):
     if opcion == 1:
         system("cls")
@@ -87,16 +97,36 @@ def comprobarOpcion(opcion):
         system("cls")
         print(cabecera)
         mostrasTareas()
-        input('\n' + "        Pulsa intro para continuar.")
+        esperar()
 
     elif opcion == 4:
         system("cls")
         print(cabecera)
         mostrasTareas()
         borrarTarea()
-        
 
-#PROGRAMA PRINCIPAL
+    elif opcion == 5:
+        system("cls")
+        print(cabecera)
+        print('''      MUCHAS GRACIAS POR USAR GESTOR DE TAREAS
+                
+                  PASA UN BUEN DÍA\n\n''')
+
+#Creamos las variables que vamos a utilizar
+opcion = 0
+lista = []
+cabecera = '''\n           PROGRAMA DE GESTIÓN DE TAREAS
+             CREADO POR ENRIQUE MUÑOZ
+        ====================================
+        '''
+menu = '''
+          1. Agregar nueva tarea
+          2. Marcar tarea como completada
+          3. Mostrar todas las tareas
+          4. Eliminar una tarea
+          5. Finalizar'''
+
+#INICIAMOS EL PROGRAMA PRINCIPAL
 while opcion!=5:
     system("cls")
     print(cabecera + menu)
@@ -107,5 +137,5 @@ while opcion!=5:
         else:
             comprobarOpcion(opcion)
     except ValueError:
-        print('\n          Tienes que marcar un numero del 1 al 5')
-        input('\n' + "          Pulsa intro para continuar.")
+        print('\n          Tienes que marcar un numero del 1 al 5 \n')
+        esperar()
